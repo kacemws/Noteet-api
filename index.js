b9d0d830dcce552126eaae8856215f63caec3359
kacemws <berras.belkacem@gmail.com>
2021-04-11 11:56:33 +0100

    adding swgger ui to the app

--- index.js
+++ index.js
@@ -12,6 +12,50 @@ const cors = require("cors");
 const InitiateMongoServer = require("./src/utils/db");
 var path = require("path");
 
+// ROUTES
+const user = require("./src/routes/user");
+const note = require("./src/routes/note");
+
+//Swagger
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Express API for Noteet",
+    version: "1.2.0",
+    description:
+      "This is a REST API application made with Express. It is the backend behind noteet, which allows user to create note in an easy way.",
+    license: {
+      name: "Licensed Under MIT",
+      url: "https://spdx.org/licenses/MIT.html",
+    },
+    contact: {
+      name: "Belkacem Berras",
+      url: "https://www.belkacember.com/",
+    },
+  },
+  servers: [
+    {
+      url: "http://localhost:8083/v1",
+      description: "Development server",
+    },
+    {
+      url: "https://noteet-api.herokuapp.com/v1",
+      description: "Production server",
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: [".src/routes/*.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
 // INIT
 InitiateMongoServer();
 const app = express();
@@ -19,13 +63,10 @@ const app = express();
 // PORT
 const port = process.env.PORT || 8083;
 
-// ROUTES
-const user = require("./src/routes/user");
-const note = require("./src/routes/note");
-
 //MIDDLEWARES
 app.use(bodyParser.json());
 app.use(cors());
+app.use("/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/v1/user", user);
 app.use("/v1/notes", note);
 
